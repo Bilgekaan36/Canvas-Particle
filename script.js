ae1c908208a4b54ebf5acf16142de12972925dcd
Bilgekaan Yilmaz <bilgekaan.yilmaz36@gmail.com>
2021-12-08 20:50:56 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,93 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let particlesArray = [];
+const numberOfParticles = 300;
+
+//measure title element
+let titleElement = document.getElementById('title1');
+titleMeasurements = titleElement.getBoundingClientRect();
+let title = {
+    x: titleMeasurements.left,
+    y: titleMeasurements.top,
+    width: titleMeasurements.width,
+    height: 10,
+};
+
+class Particle {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.size = Math.random() * 15 + 1;
+        this.weight = Math.random() * 1 + 1;
+        this.directionX = (Math.random() * 2) -1;
+    }
+    update() {
+        console.log('update start')
+        if (this.y > canvas.height) {
+            this.y = 0 - this.size;
+            this.weight = Math.random() * 1 + 1;
+            this.x = Math.random() * canvas.width * 1.3;
+        }
+        this.weight += 0.01;
+        this.y += this.weight;
+        this.x += this.directionX;
+
+        //check for collision between each particle and title element
+        if (
+            this.x < title.x + title.width &&
+            this.x + this.size > title.x &&
+            this.y < title.y + title.height &&
+            this.y + this.size > title.y
+        ) {
+            this.y -= 3;
+            this.weight *= -0.5;
+        }
+    }
+    draw() {
+        console.log('draw start')
+        ctx.fillStyle = '#005972';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.closePath();
+        ctx.fill();
+    }
+}
+function init() {
+    console.log('init start')
+    particlesArray = [];
+    for (let i = 0; i < numberOfParticles; i++) {
+        const x = Math.random() * canvas.width;
+        const y = Math.random() * canvas.height;
+        particlesArray.push(new Particle(x, y));
+    }
+}
+init();
+
+function animate() {
+    console.log('animate start')
+    ctx.fillStyle = 'rgba(255, 255, 255, 0.01)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < particlesArray.length; i++) {
+        particlesArray[i].update();
+        particlesArray[i].draw();
+    }
+    requestAnimationFrame(animate);
+}
+animate();
+//----------------------------------------------- only resize window -----------------------------------
+window.addEventListener('resize', function () {
+    console.log('eventlistener start')
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    titleMeasurements = titleElement.getBoundingClientRect();
+    title = {
+        x: titleMeasurements.left,
+        y: titleMeasurements.top,
+        width: titleMeasurements.width,
+        height: 10,
+    };
+    init();
+})
